2f148214732fdedeeca2588d2a1716a299d87dda
jiangjian <jiangjian199204@gmail.com>
2021-01-26 14:57:48 +0800

    format

--- webpack.config.js
+++ webpack.config.js
@@ -3,33 +3,33 @@ const path = require("path");
 
 module.exports = {
 	entry: {
-		index: path.resolve(__dirname, "src", "index.js")
+		index: path.resolve(__dirname, "src", "index.js"),
 	},
 	output: {
-		path: path.resolve(__dirname, "dist")
+		path: path.resolve(__dirname, "dist"),
 	},
 	module: {
 		rules: [
 			{
 				test: /\.css$/,
-				use: ["style-loader", "css-loader"]
+				use: ["style-loader", "css-loader"],
 			},
 			{
 				test: /\.scss$/,
-				use: ["style-loader", "css-loader", "sass-loader"]
+				use: ["style-loader", "css-loader", "sass-loader"],
 			},
 			{
 				test: /\.js$/,
-				use: ["babel-loader"]
-			}
-		]
+				use: ["babel-loader"],
+			},
+		],
 	},
 	plugins: [
 		new HtmlWebpackPlugin({
-			template: path.resolve(__dirname, "src", "index.html")
-		})
+			template: path.resolve(__dirname, "src", "index.html"),
+		}),
 	],
 	optimization: {
-		splitChunks: { chunks: "all" }
-	}
+		splitChunks: { chunks: "all" },
+	},
 };
