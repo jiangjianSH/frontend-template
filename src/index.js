14fa46e00e7f9b09f2edc22848e38c6ee44da346
jiangjian <jiangjian199204@gmail.com>
2021-01-31 15:26:15 +0800

    init webpack project

--- src/index.js
+++ src/index.js
@@ -1,16 +1,1 @@
-import "./style.css";
-import "./style.scss";
-
-const getUserModule = () => import("./common/usersAPI");
-
-var hello = (name) => console.log(`hello ${name}`);
-
-hello("jiangjianxx");
-
-const btn = document.getElementById("btn");
-
-btn.addEventListener("click", () => {
-	getUserModule().then(({ getUsers }) => {
-		getUsers().then((json) => console.log(json));
-	});
-});
+console.log("hello");
